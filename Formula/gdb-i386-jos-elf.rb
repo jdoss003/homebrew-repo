5ae241b3ccd979275985954075010728578c12bf
jdoss003 <jdoss003@ucr.edu>
2018-04-03 16:14:22 -0700

    Add gdb-i386-jos-elf

---
+++ Formula/gdb-i386-jos-elf.rb
@@ -0,0 +1,58 @@
+class GdbI386JosElf < Formula
+
+  # Modified version of Homebrew's gdb formula
+
+  desc "GNU debugger"
+  homepage "https://www.gnu.org/software/gdb/"
+  url "https://ftp.gnu.org/gnu/gdb/gdb-8.1.tar.xz"
+  mirror "https://ftpmirror.gnu.org/gdb/gdb-8.1.tar.xz"
+  sha256 "af61a0263858e69c5dce51eab26662ff3d2ad9aa68da9583e8143b5426be4b34"
+
+  depends_on "gcc-i386-jos-elf"
+
+  fails_with :clang do
+    build 800
+    cause <<~EOS
+      probe.c:63:28: error: default initialization of an object of const type
+      'const any_static_probe_ops' without a user-provided default constructor
+    EOS
+  end
+
+  fails_with :clang do
+    build 600
+    cause <<~EOS
+      clang: error: unable to execute command: Segmentation fault: 11
+      Test done on: Apple LLVM version 6.0 (clang-600.0.56) (based on LLVM 3.5svn)
+    EOS
+  end
+
+  def install
+    args = [
+      "--prefix=#{prefix}",
+      "--target=i386-jos-elf",
+      "--program-prefix=i386-jos-elf-",
+      "--disable-werror"
+    ]
+
+    system "./configure", *args
+    system "make"
+
+    # Don't install bfd or opcodes, as they are provided by binutils
+    inreplace ["bfd/Makefile", "opcodes/Makefile"], /^install:/, "dontinstall:"
+
+    system "make", "all"
+    system "make", "install"
+  end
+
+  def caveats; <<~EOS
+    gdb requires special privileges to access Mach ports.
+    You will need to codesign the binary. For instructions, see:
+
+      https://sourceware.org/gdb/wiki/BuildingOnDarwin
+
+    On 10.12 (Sierra) or later with SIP, you need to run this:
+
+      echo "set startup-with-shell off" >> ~/.gdbinit
+    EOS
+  end
+end
